e19ad959109364e8844cfc5f2072d0d576dac922
drsprite <pat@patriccio.com>
2015-07-05 10:54:10 -0400

    Rewrite of station status. More config options.
    This is a big update since the initial release of the script:
    Re-wrote the entire station notification to be able to notify when the
    station has turned off.
    Added more options to the config file, such as individual notify on/off
    and also
    the ability to customize individual notification messages.
    Added water level notification.
    Added more verbose logging.
    Lastly, re-organized the functions and main loop code.

--- ospi_push_notifications.py
+++ ospi_push_notifications.py
@@ -7,10 +7,17 @@
 	
 	If the script crashes, it will log to the syslog and send an email to let you know it's crashed. 
 	
-	6/26/2015, Pat O'Brien. Licensed under the MIT License.
+	6/26/2015, Pat O'Brien. Licensed under the MIT License. Initial release.
 	6/30/2015, added Pushover as a push notification service.
 	7/2/2015, added rain sensor notifications, split out options to a config file
 				and added more logging for troubleshooting	
+	7/5/2015, This is a big update since the initial release of the script:
+				Re-wrote the entire station notification to be able to notify when the station has turned off.
+				Added more options to the config file, such as individual notify on/off and also
+					the ability to customize individual notification messages. 
+				Added water level notification.
+				Added more verbose logging. 
+				Lastly, re-organized the functions and main loop code.
 	"""
 
 import os, syslog, urllib2, json, requests, yaml
@@ -37,7 +44,7 @@ try:
 	with open(config_path, 'r') as file:
 		config = yaml.load(file)
 except:
-	syslog.syslog("Unable to load %s. Check the file and try again." % config_path)
+	syslog.syslog("Unable to load %s. Check the file exists and try again." % config_path)
 
 # Setup variables from config file
 ospiPort = config["ospi"]["port"]
@@ -53,29 +60,12 @@ smtpServer = config["email"]["server"]
 subject = config["email"]["subject"]
 text = config["email"]["subject"]
 
-# Get Rain Sensor status
-def getRainSensorStatus():
-	try:
-		ospiRainSensorStatus = urllib2.urlopen("http://localhost:" + ospiPort + "/jc?pw=" + ospiApiPasswordHash).read()
-	except:
-		syslog.syslog("Unable to load the OSPi API URL for Rain Sensor Status. You might have a bad hashed password.")
-		sendEmail()
-	
-	try:
-		data = json.loads(ospiRainSensorStatus)
-	except:
-		syslog.syslog("Unable to parse OSPi Rain Sensor Status JSON Output.")
-		sendEmail()
-		
-	rainSensor = data["rs"]
-	return rainSensor
-
 # Get Station Status
-def getStatus():
+def getStationStatus():
 	try:
 		ospiStationStatus = urllib2.urlopen("http://localhost:" + ospiPort + "/js?pw=" + ospiApiPasswordHash).read()
 	except:
-		syslog.syslog("Unable to load the OSPi API URL for Station Status. You might have a bad hashed password.")
+		syslog.syslog("Unable to load the OSPi API URL for Station Status. You might have a bad hashed password or the OSPi is not online.")
 		sendEmail()
 	
 	try:
@@ -88,17 +78,60 @@ def getStatus():
 	#print "Getting sprinkler status. Zones defined: %s. Zone data: %s" % (data["nstations"],data["sn"])
 	return stations
 
-# Send Push Notification
-def sendPushNotification(type, zoneID):
-	# Change verbiage based on event type
-	if (type == "station_active"):
-		event = "Zone %s is now active" % zoneID
-		
-	elif (type == "rainSensor_active"):
-		event = "Rain sensor has detected rain"
+# Get Rain Sensor status
+def getRainSensorStatus():
+	try:
+		ospiRainSensorStatus = urllib2.urlopen("http://localhost:" + ospiPort + "/jc?pw=" + ospiApiPasswordHash).read()
+	except:
+		syslog.syslog("Unable to load the OSPi API URL for Rain Sensor Status. You might have a bad hashed password or the OSPi is not online.")
+		sendEmail()
 	
-	elif (type == "rainSensor_clear"):
-		event = "Rain sensor has cleared"
+	try:
+		data = json.loads(ospiRainSensorStatus)
+	except:
+		syslog.syslog("Unable to parse OSPi Rain Sensor Status JSON Output.")
+		sendEmail()
+		
+	rainSensor = data["rs"]
+	return rainSensor
+
+# Get the watering level
+def getWaterLevel():
+	try:
+		ospiWaterLevel = urllib2.urlopen("http://localhost:" + ospiPort + "/jo?pw=" + ospiApiPasswordHash).read()
+	except:
+		syslog.syslog("Unable to load the OSPi API URL for Water Level. You might have a bad hashed password or the OSPi is not online.")
+		sendEmail()
+	
+	try:
+		data = json.loads(ospiWaterLevel)
+	except:
+		syslog.syslog("Unable to parse OSPi Water Level JSON Output.")
+		sendEmail()
+
+	waterLevel = data["wl"]
+	#print "Water level currently is %s%%" % waterLevel
+	return waterLevel
+
+# Send Push Notification
+def sendPushNotification(notifyType, notifyInfo):
+	# Change verbiage based on event type
+	if (notifyType == "station_active"):
+		#event = "Zone %s is now active" % notifyInfo
+		event = config["stations"]["messages"]["start"].format(notifyInfo)
+		
+	elif (notifyType == "station_idle"):
+		#event = "Zone %s is now idle" % notifyInfo
+		event = config["stations"]["messages"]["stop"].format(notifyInfo)
+		
+	elif (notifyType == "rainSensor_active"):
+		event = config["rain"]["messages"]["active"]
+	
+	elif (notifyType == "rainSensor_clear"):
+		event = config["rain"]["messages"]["clear"]
+		
+	elif (notifyType == "waterLevel"):
+		event = config["waterlevel"]["message"].format(notifyInfo)
 	
 	
 	if (pushService == "instapush"):
@@ -125,46 +158,66 @@ def sendPushNotification(type, zoneID):
 
 # Main loop to check the status and send notification if necessary	
 def main():
-	syslog.syslog('Push notification script started')
-	notifyZone = 0
-	notifySent = "n"
+	syslog.syslog('OSPi push notification script started.')
 	currentStation = 0
 	currentRainStatus = 0
+	currentWaterLevel = 0
 	
-	while True:
-		# Get the station & rain status from the controller API
-		rainSensor = getRainSensorStatus()
-		stations = getStatus()
+	try:
+		while True:
 		
-		# Rain sensor status
-		if (rainSensor == 1): # Rain sensor active
-			if (currentRainStatus == 0): # Still showing no rain, send notification and set rain detected
-				currentRainStatus = 1
-				sendPushNotification("rainSensor_active", 0)
-				
-		elif (rainSensor == 0): # Rain sensor not active
-			if (currentRainStatus == 1): # Still showing rain. Send an all clear notification and clear the rain detected
-				currentRainStatus = 0
-				sendPushNotification("rainSensor_clear", 0)
+			# Station zone status
+			stations = getStationStatus()
+			i = 1
+			for zoneStatus in stations:
+				if (zoneStatus == 1):
+					if (currentStation != i):
+						# Zone change detected. Send notification that previous zone stopped, except if previous zone was 0
+						if ( (currentStation != 0) & (config["stations"]["notify"]["stop"] == "yes") ):
+							syslog.syslog("Station has gone idle: %s" % currentStation)
+							sendPushNotification("station_idle", currentStation)
+						currentStation = i
+						# New zone is active, send notification
+						if (config["stations"]["notify"]["start"] == "yes"):
+							syslog.syslog("Station is now active: %s" % i)
+							sendPushNotification("station_active", i)
 
-				
-		# Station zone status
-		i = 1
-		for zoneStatus in stations:
-			if (zoneStatus == 1):
-				#print "Zone %s is active" % i
-				if ( (notifyZone == i) & (notifySent == "y") ):
-					#print "Push notification already sent"
-					tempVar = "blah"
-				else:
-					sendPushNotification("station_active", i)
-					notifyZone = i
-					notifySent = "y"
-					#print "Sending push notification to %s" % pushService
+				elif ( (zoneStatus == 0) & (currentStation == i) ):
+					# All stations off, including a previously-on station. Send idle notification, and reset station to 0
+					if ( (currentStation != 0) & (config["stations"]["notify"]["stop"] == "yes") ):
+						syslog.syslog("Station has gone idle: %s" % currentStation)
+						sendPushNotification("station_idle", currentStation)
+						currentStation = 0
+						
+				i = i + 1
+
+			# Rain sensor status & notifications
+			if (config["rain"]["notify"] == "yes"):
+				rainSensor = getRainSensorStatus()
+				if (rainSensor == 1): # Rain sensor active
+					if (currentRainStatus == 0): # We showed no rain, now we have rain, send notification and set rain detected
+						currentRainStatus = 1
+						syslog.syslog("Rain sensor is now active")
+						sendPushNotification("rainSensor_active", 0)
+				elif ( (config["rain"]["notify"] == "yes") & (rainSensor == 0) ): # Rain sensor not active
+					if (currentRainStatus == 1): # We showed rain, now we have no rain. Send an all clear notification and clear the rain detected
+						currentRainStatus = 0
+						syslog.syslog("Rain sensor has cleared")
+						sendPushNotification("rainSensor_clear", 0)
+
+			# Water level notifications
+			if (config["waterlevel"]["notify"] == "yes"):
+				waterLevel = getWaterLevel()
+				if (currentWaterLevel != waterLevel):
+					# New water level detected
+					currentWaterLevel = waterLevel
+					syslog.syslog("Water level has changed to: %s" % waterLevel)
+					sendPushNotification("waterLevel", waterLevel)
 					
-			i = i + 1
-		
-		sleep(5)
+
+			sleep(5)
+	except:
+		syslog.syslog("OSPi push notification script stopped.")
 
 if __name__ == '__main__':
 	main()
